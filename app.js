7c60a0cf28771f7b28035a1f57ee7047ae5c7beb
Anil Kumar Behera <ark845612@gmail.com>
2023-03-01 13:32:39 +0530

    delete and redirect

--- app.js
+++ app.js
@@ -125,6 +125,18 @@ app.get("/blogs/:id",(req,res)=>{
     .catch((err)=>{
         console.log(err);
     });
+});
+
+app.delete('/blogs/:id',(req,res)=>{
+    const id=req.params.id;
+
+    Blog.findByIdAndDelete(id)
+    .then(result=>{
+        res.json({redirect:'/blogs'});
+    })
+    .catch(err=>{
+        console.log(err);
+    })
 })
 app.get('/blogs/create',(req,res)=>{
     // res.send('<p>About Page</p>');
