718a9b53812f1c78b9143c20d1126a054420f756
Anil Kumar Behera <ark845612@gmail.com>
2023-03-01 11:58:31 +0530

    post request

--- app.js
+++ app.js
@@ -116,6 +116,16 @@ app.post('/blogs',(req,res)=>{
     })
 })
 
+app.get("/blogs/:id",(req,res)=>{
+    const id=req.params.id;
+    Blog.findById(id)
+    .then((result)=>{
+        res.render('details',{title:'Blog Details',blog:result})
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+})
 app.get('/blogs/create',(req,res)=>{
     // res.send('<p>About Page</p>');
     // res.sendFile('./views/about.html',{root:__dirname});
